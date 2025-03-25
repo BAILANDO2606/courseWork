eb09d0cdfcbb56f1f76b286f594e8e1468359dd6
BAILANDO2606 <glasklar21@gmail.com>
2025-03-25 23:11:24 +0200

    change context

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,42 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import { FaDiscord, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
-];
+	{ href: 'https://discord.gg/B7fTeKme', icon: <FaDiscord /> },
+	{ href: 'https://x.com/bailando2606', icon: <FaTwitter /> },
+	{ href: 'https://www.youtube.com/@BAILANDO-w4f', icon: <FaYoutube /> },
+]
 
 const Footer = () => {
-  return (
-    <footer className="w-screen bg-[#5542ff] py-4 text-black">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
-        <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
-        </p>
+	return (
+		<footer className='w-screen bg-[#5542ff] py-4 text-black'>
+			<div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
+				<p className='text-center text-sm font-light md:text-left'>
+					©Privacy 2025. All rights reserved
+				</p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
-          {socialLinks.map((link, index) => (
-            <a
-              key={index}
-              href={link.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
-            >
-              {link.icon}
-            </a>
-          ))}
-        </div>
+				<div className='flex justify-center gap-4  md:justify-start'>
+					{socialLinks.map((link, index) => (
+						<a
+							key={index}
+							href={link.href}
+							target='_blank'
+							rel='noopener noreferrer'
+							className='text-black transition-colors duration-500 ease-in-out hover:text-white'
+						>
+							{link.icon}
+						</a>
+					))}
+				</div>
 
-        <a
-          href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
-        >
-          Privacy Policy
-        </a>
-      </div>
-    </footer>
-  );
-};
+				<a
+					href='#privacy-policy'
+					className='text-center text-sm font-light hover:underline md:text-right'
+				>
+					Privacy Policy
+				</a>
+			</div>
+		</footer>
+	)
+}
 
-export default Footer;
+export default Footer
